1798082131ffdcd1ffa35c03b4ec93e34007c959
ceo-aivocado <ceo-aivocado@users.noreply.github.com>
2026-09-16 23:23:03 +0300

    Enlarge movement buttons and double their gap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sabbath-mobile-v4';
+const CACHE='sabbath-mobile-v5';
 const FILES=['./index.html','./pwa.js?v=3','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(names=>Promise.all(names.filter(name=>name.startsWith('sabbath-mobile-')&&name!==CACHE).map(name=>caches.delete(name)))).then(()=>self.clients.claim())));
