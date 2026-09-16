1456983cba309097d4efb318dee8deb4808c98b6
ceo-aivocado <ceo-aivocado@users.noreply.github.com>
2026-09-16 23:13:38 +0300

    Arrange controls in diamond, block game zoom gestures, update installed app in place

--- pwa.js
+++ pwa.js
@@ -1,5 +1,10 @@
 if('serviceWorker' in navigator && ['https:','http:'].includes(location.protocol)){
- const wasControlled=!!navigator.serviceWorker.controller;let refreshed=false;
- navigator.serviceWorker.addEventListener('controllerchange',()=>{if(wasControlled&&!refreshed){refreshed=true;location.reload()}});
- navigator.serviceWorker.register('./sw.js',{updateViaCache:'none'}).then(reg=>reg.update()).catch(()=>{});
+ let hadController=!!navigator.serviceWorker.controller,pending=false,reloading=false,registration;
+ function applyUpdate(){if(pending&&!reloading&&window.sabbath?.game.mode!=='playing'){reloading=true;location.reload()}}
+ function checkUpdate(){if(document.hidden)return;applyUpdate();registration?.update().catch(()=>{})}
+ navigator.serviceWorker.addEventListener('controllerchange',()=>{if(hadController){pending=true;applyUpdate()}hadController=true;});
+ navigator.serviceWorker.register('./sw.js',{updateViaCache:'none'}).then(reg=>{registration=reg;checkUpdate()}).catch(()=>{});
+ window.addEventListener('pageshow',checkUpdate);window.addEventListener('online',checkUpdate);
+ document.addEventListener('visibilitychange',checkUpdate);
+ setInterval(checkUpdate,60000);
 }
